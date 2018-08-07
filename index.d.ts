6bbf3a4a3696065a09ed81608bff13c398358a62
Martin Kutter <kutter@chemmedia.de>
2018-08-07 14:38:36 +0200

    adding initial implementation

---
+++ index.d.ts
@@ -0,0 +1,4 @@
+declare module 'knowledgeworker-embedded-asset-api' {
+    export function setHeight(height: number): void;
+    export function setOnInteractionComplete(complete: boolean): void;
+}
