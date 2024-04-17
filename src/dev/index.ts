705eccf313567f5cfb58f324d6832c1ea8974419
Bettina Helgenlechner <helgenlechner@chemmedia.de>
2024-04-17 11:38:30 +0200

    fix dev environment condition

--- src/dev/index.ts
+++ src/dev/index.ts
@@ -1,7 +1,7 @@
 import { setDesign } from '../design.ts';
 import { defaultDesign, getStyles } from 'knowledgeworker-embedded-asset-api-ui';
 
-if (import.meta.env.DEV) {
+if (process.env.NODE_ENV === 'development') {
     // initialize design in development mode so developers can see the theme without a containing environment
     setDesign(getStyles(defaultDesign));
 }
